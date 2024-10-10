7a1486aa2c514d499ef4581d78be146278944cbd
swatibadola <sb3875@dseu.ac.in>
2024-10-10 14:03:09 +0530

    pages and routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 function App() {
   return (
-    <div className=' text-4xl p-5 text-orange-800 '>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'
+        element={<Home/>}/>
+        <Route path='/about'
+        element={<About/>}/>
+        <Route path='/sign-in'
+        element={<Signin/>}/>
+        <Route path='/sign-up'
+        element={<Signup/>}/>
+        <Route path='/dashboard'
+        element={<Dashboard/>}/>
+        <Route path='/projects'
+        element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
